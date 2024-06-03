67ff6d6d79c94e64b236b461807035c557ff3c4e
Leonarotti <159678359+Leonarotti@users.noreply.github.com>
2024-06-02 22:47:54 -0600

    CRUD Coupon

--- coupons-admin/src/components/couponManagement/CouponManagement.js
+++ coupons-admin/src/components/couponManagement/CouponManagement.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import couponService from '../../services/CouponService';
+import categoryService from '../../services/CategoryService'; // Importar el servicio de categorías
 import CouponModal from '../couponModal/CouponModal';
 import './CouponManagement.css';
 
@@ -8,14 +9,20 @@ const CouponManagement = () => {
     const { enterpriseId } = useParams();
     const navigate = useNavigate();
     const [coupons, setCoupons] = useState([]);
+    const [categories, setCategories] = useState([]); // Estado para almacenar las categorías
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [editMode, setEditMode] = useState(false);
     const [newCoupon, setNewCoupon] = useState({
-        title: '',
-        description: '',
-        discount: '',
-        expiration_date: '',
-        enterprise_id: enterpriseId
+        id_enterprise: enterpriseId,
+        id_category: '',
+        name: '',
+        img: '',
+        location: '',
+        regular_price: '',
+        percentage: '',
+        start_date: '',
+        end_date: '',
+        is_enabled: true
     });
     const [backendErrors, setBackendErrors] = useState({});
 
@@ -27,13 +34,22 @@ const CouponManagement = () => {
         });
     }, [enterpriseId]);
 
+    const loadCategories = useCallback(() => {
+        categoryService.getCategories().then(response => {
+            setCategories(response.data);
+        }).catch(error => {
+            console.error("Error fetching categories:", error);
+        });
+    }, []);
+
     useEffect(() => {
         loadCoupons();
-    }, [loadCoupons]);
+        loadCategories();
+    }, [loadCoupons, loadCategories]);
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNewCoupon({ ...newCoupon, [name]: value });
+        const { name, value, type, checked } = e.target;
+        setNewCoupon({ ...newCoupon, [name]: type === 'checkbox' ? checked : value });
     };
 
     const handleCreateOrUpdate = (e) => {
@@ -49,7 +65,7 @@ const CouponManagement = () => {
                 }
             }).catch(error => {
                 console.error("Error updating coupon:", error);
-                setBackendErrors(error); // Maneja el error y lo muestra en el componente
+                setBackendErrors(error.response.data); // Maneja el error y lo muestra en el componente
             });
         } else {
             couponService.createCoupon(newCoupon).then(response => {
@@ -62,7 +78,7 @@ const CouponManagement = () => {
                 }
             }).catch(error => {
                 console.error("Error creating coupon:", error);
-                setBackendErrors(error); // Maneja el error y lo muestra en el componente
+                setBackendErrors(error.response.data); // Maneja el error y lo muestra en el componente
             });
         }
     };
@@ -88,11 +104,16 @@ const CouponManagement = () => {
     const closeModal = () => {
         setModalIsOpen(false);
         setNewCoupon({
-            title: '',
-            description: '',
-            discount: '',
-            expiration_date: '',
-            enterprise_id: enterpriseId
+            id_enterprise: enterpriseId,
+            id_category: '',
+            name: '',
+            img: '',
+            location: '',
+            regular_price: '',
+            percentage: '',
+            start_date: '',
+            end_date: '',
+            is_enabled: true
         });
         setEditMode(false);
         setBackendErrors({});
@@ -112,20 +133,28 @@ const CouponManagement = () => {
             <table>
                 <thead>
                     <tr>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Discount</th>
-                        <th>Expiration Date</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Location</th>
+                        <th>Regular Price</th>
+                        <th>Discount (%)</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Enabled</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {coupons.map(coupon => (
                         <tr key={coupon.id_coupon}>
-                            <td>{coupon.title}</td>
-                            <td>{coupon.description}</td>
-                            <td>{coupon.discount}</td>
-                            <td>{coupon.expiration_date}</td>
+                            <td>{coupon.name}</td>
+                            <td>{coupon.id_category}</td>
+                            <td>{coupon.location}</td>
+                            <td>{coupon.regular_price}</td>
+                            <td>{coupon.percentage}</td>
+                            <td>{coupon.start_date}</td>
+                            <td>{coupon.end_date}</td>
+                            <td>{coupon.is_enabled ? 'Yes' : 'No'}</td>
                             <td>
                                 <button onClick={() => handleEdit(coupon)}>Edit</button>
                                 <button onClick={() => handleDelete(coupon.id_coupon)}>Delete</button>
@@ -143,6 +172,7 @@ const CouponManagement = () => {
                 handleSubmit={handleCreateOrUpdate}
                 editMode={editMode}
                 backendErrors={backendErrors} // Pasar los errores del backend al modal
+                categories={categories} // Pasar las categorías al modal
             />
         </div>
     );
--- coupons-admin/src/components/couponModal/CouponModal.js
+++ coupons-admin/src/components/couponModal/CouponModal.js
@@ -2,26 +2,38 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 import './Modal.css';
 
-const CouponModal = ({ isOpen, onRequestClose, coupon, handleChange, handleSubmit, editMode, backendErrors }) => {
+const CouponModal = ({ isOpen, onRequestClose, coupon, handleChange, handleSubmit, editMode, backendErrors, categories }) => {
     const [errors, setErrors] = useState({});
 
     const validateForm = () => {
         const errors = {};
 
-        if (!coupon.title.trim()) {
-            errors.title = 'Title is required';
+        if (!coupon.name.trim()) {
+            errors.name = 'Name is required';
         }
 
-        if (!coupon.description.trim()) {
-            errors.description = 'Description is required';
+        if (!coupon.id_category.trim()) {
+            errors.id_category = 'Category is required';
         }
 
-        if (!/^(\d+)(\.\d{1,2})?$/.test(coupon.discount.trim())) {
-            errors.discount = 'Invalid discount format. Should be a number with up to two decimal places.';
+        if (!coupon.location.trim()) {
+            errors.location = 'Location is required';
         }
 
-        if (!coupon.expiration_date.trim()) {
-            errors.expiration_date = 'Expiration date is required';
+        if (!/^(\d+)(\.\d{1,2})?$/.test(coupon.regular_price.trim())) {
+            errors.regular_price = 'Invalid regular price format. Should be a number with up to two decimal places.';
+        }
+
+        if (!/^\d{1,2}$/.test(coupon.percentage) || coupon.percentage < 0 || coupon.percentage > 100) {
+            errors.percentage = 'Invalid percentage format. Should be a number between 0 and 100.';
+        }
+
+        if (!coupon.start_date.trim()) {
+            errors.start_date = 'Start date is required';
+        }
+
+        if (!coupon.end_date.trim()) {
+            errors.end_date = 'End date is required';
         }
 
         setErrors(errors);
@@ -37,24 +49,50 @@ const CouponModal = ({ isOpen, onRequestClose, coupon, handleChange, handleSubmi
                     handleSubmit(e);
                 }
             }}>
-                <input type="text" name="title" placeholder="Title" value={coupon.title} onChange={handleChange} required />
-                {errors.title && <span className="error">{errors.title}</span>}
-                {backendErrors.title && <span className="error">{backendErrors.title}</span>}
+                <input type="text" name="name" placeholder="Name" value={coupon.name} onChange={handleChange} required />
+                {errors.name && <span className="error">{errors.name}</span>}
+                {backendErrors.name && <span className="error">{backendErrors.name}</span>}
 
-                <input type="text" name="description" placeholder="Description" value={coupon.description} onChange={handleChange} required />
-                {errors.description && <span className="error">{errors.description}</span>}
-                {backendErrors.description && <span className="error">{backendErrors.description}</span>}
+                <select name="id_category" value={coupon.id_category} onChange={handleChange} required>
+                    <option value="">Select Category</option>
+                    {categories.map(category => (
+                        <option key={category.id_category} value={category.id_category}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+                {errors.id_category && <span className="error">{errors.id_category}</span>}
+                {backendErrors.id_category && <span className="error">{backendErrors.id_category}</span>}
 
-                <input type="text" name="discount" placeholder="Discount" value={coupon.discount} onChange={handleChange} required />
-                {errors.discount && <span className="error">{errors.discount}</span>}
-                {backendErrors.discount && <span className="error">{backendErrors.discount}</span>}
+                <input type="text" name="location" placeholder="Location" value={coupon.location} onChange={handleChange} required />
+                {errors.location && <span className="error">{errors.location}</span>}
+                {backendErrors.location && <span className="error">{backendErrors.location}</span>}
 
-                <input type="date" name="expiration_date" placeholder="Expiration Date" value={coupon.expiration_date} onChange={handleChange} required />
-                {errors.expiration_date && <span className="error">{errors.expiration_date}</span>}
-                {backendErrors.expiration_date && <span className="error">{backendErrors.expiration_date}</span>}
+                <input type="text" name="regular_price" placeholder="Regular Price" value={coupon.regular_price} onChange={handleChange} required />
+                {errors.regular_price && <span className="error">{errors.regular_price}</span>}
+                {backendErrors.regular_price && <span className="error">{backendErrors.regular_price}</span>}
 
-                <button type="submit">{editMode ? 'Update' : 'Create'}</button>
-                <button type="button" onClick={onRequestClose}>Cancel</button>
+                <input type="text" name="percentage" placeholder="Discount Percentage" value={coupon.percentage} onChange={handleChange} required />
+                {errors.percentage && <span className="error">{errors.percentage}</span>}
+                {backendErrors.percentage && <span className="error">{backendErrors.percentage}</span>}
+
+                <input type="date" name="start_date" value={coupon.start_date} onChange={handleChange} required />
+                {errors.start_date && <span className="error">{errors.start_date}</span>}
+                {backendErrors.start_date && <span className="error">{backendErrors.start_date}</span>}
+
+                <input type="date" name="end_date" value={coupon.end_date} onChange={handleChange} required />
+                {errors.end_date && <span className="error">{errors.end_date}</span>}
+                {backendErrors.end_date && <span className="error">{backendErrors.end_date}</span>}
+
+                <label>
+                    <input type="checkbox" name="is_enabled" checked={coupon.is_enabled} onChange={handleChange} />
+                    Is Enabled
+                </label>
+
+                <div className="modal-buttons">
+                    <button type="submit">{editMode ? 'Update' : 'Create'}</button>
+                    <button type="button" onClick={onRequestClose}>Cancel</button>
+                </div>
             </form>
         </Modal>
     );
