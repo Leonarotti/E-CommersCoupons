9464621807799fe5465b569df028379f28f00553
Leonarotti <159678359+Leonarotti@users.noreply.github.com>
2024-06-11 17:25:28 -0600

    Create Masking.cs

---
+++ CouponsClientBackendC#/CouponsClient.BC/BusinessLogic/Masking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouponsClient.BC.BusinessLogic
+{
+    public class Masking
+    {
+        public static string MaskCardNumber(string cardNumber)
+        {
+            if (string.IsNullOrEmpty(cardNumber) || cardNumber.Length < 4)
+            {
+                return string.Empty;
+            }
+
+            return string.Concat("**** **** **** ", cardNumber.Substring(cardNumber.Length - 4));
+        }
+    }
+}
